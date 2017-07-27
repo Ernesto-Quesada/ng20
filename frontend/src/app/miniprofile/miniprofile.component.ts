d2deb25d20ec73f1c2ef82b3b83bd5d0101c0faa
norbeqr <norbeqr@gmail.com>
2017-07-26 22:39:42 -0500

    fixed miniprofile view

--- frontend/src/app/miniprofile/miniprofile.component.ts
+++ frontend/src/app/miniprofile/miniprofile.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class MiniprofileComponent implements OnInit {
   user: any = {
-    agencyInUseId: {}
+     agencyInUseId: {}
   };
   errorMessage: any;
 
@@ -21,6 +21,7 @@ ngOnInit() {
 
         this.mySessionService.getProfile()
               .then((theUsercomingFromApi) => {
+                console.log("asdasdasdasdasdasdas"+theUsercomingFromApi.theUserProfile.firstName)
               this.user = theUsercomingFromApi.theUserProfile;
               this.errorMessage = null;
               this.routetheuser.navigate(['/portal']);
